2e6d40b89eb262235ab029222fbad1f23ed9278b
Alexander <s555b@mail.ru>
2023-11-09 21:13:18 +0300

    API Update

--- src/FileSystem.py
+++ src/FileSystem.py
@@ -1,11 +1,35 @@
+from pydantic import BaseModel
+from hashlib import sha512
 import json
 import os
+
+
+class Login(BaseModel):
+	login: str
+	password: str
+
+class Registration(BaseModel):
+	fullName: str
+	company: str
+	phone: str
+	email: str
+	password: str
+	gender: str
+
+class Questionnaire(BaseModel):
+	fullName: str
+	company: str
+	phone: str
+	email: str
+
 
 class FileSystem():
 	def __init__(self):
 		self.__increment = -1
 		try:
-			os.mkdir('questionnaires')
+			os.mkdir('data')
+			os.mkdir('data/accounts')
+			os.mkdir('data/questionnaires')
 		except:
 			pass
 
@@ -13,6 +37,29 @@ class FileSystem():
 		self.__increment += 1
 		return self.__increment
 
-	def writeFileSync(self, data: dict):
-		with open(f'questionnaires/ID_{self.getIncrement()}.json', 'w') as f:
-			json.dump(data, f)
+	def writeQuestionnaireSync(self, data: Questionnaire):
+		with open(f'data/questionnaires/{data.email}.json', 'w') as f:
+			f.writelines(json.dumps({
+				"fullName": data.fullName,
+				"company": data.company,
+				"phone": data.phone,
+				"email": data.email
+			}))
+
+	def writeAccountSync(self, data: Registration):
+		with open(f'data/accounts/{data.email}.json', 'w') as f:
+			f.writelines(json.dumps({
+				"fullName": data.fullName,
+				"company": data.company,
+				"phone": data.phone,
+				"email": data.email,
+				"password": sha512(bytes(data.password, encoding='utf8')).hexdigest(),
+				"gender": data.gender
+			}))
+
+	def readAccountSync(self, login: str):
+		try:
+			with open(f'data/accounts/{login}.json') as f:
+				return json.loads(*f.readlines())
+		except:
+			return {}
--- src/router.py
+++ src/router.py
@@ -1,14 +1,31 @@
-from src.FileSystem import FileSystem
+from src.FileSystem import Login, Registration, Questionnaire, FileSystem
+from pydantic import BaseModel
+from hashlib import sha512
+
+class Status(BaseModel):
+	status: bool
 
 def router(application):
 	fs = FileSystem()
+	
+	@application.post('/login')
+	async def post_login(user: Login) -> Status:
+		userData = fs.readAccountSync(user.login)
+		if (userData == {}):
+			return { 'status': False }
+		else:
+			tempPassword = sha512(bytes(user.password, encoding='utf8')).hexdigest()
+			return { 'status': userData['password'] == tempPassword }
 
-	@application.put('/')
-	async def put_root(full_name: str, company: str, phone_number: str, email: str):
-		fs.writeFileSync({
-			'full_name': full_name,
-			'company': company,
-			'phone_number': phone_number,
-			'email': email
-		})
-		return { 'status': 'OK' }
+	@application.post('/registration')
+	async def post_registration(user: Registration) -> Login:
+		fs.writeAccountSync(user)
+		return {
+			"login": user.email,
+			"password": user.password
+		}
+
+	@application.post('/questionnaire')
+	async def put_questionnaire(questionnaire: Questionnaire) -> Status:
+		fs.writeQuestionnaireSync(questionnaire)
+		return { 'status': True }
